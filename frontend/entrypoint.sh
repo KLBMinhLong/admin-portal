c0fb7f738b7125229600520c7769451b2dc9beee
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2026-05-04 09:34:12 +0700

    feat: add environment variable support to frontend container and introduce entrypoint script

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,13 +4,14 @@
 API_KEY=${API_KEY:-"changeme"}
 ENCRYPT_SECRET=${ENCRYPT_SECRET:-"encryptkey_changeme_32chars_1234"}
 
-# Thay thế các placeholder trong tất cả các file .js được sinh ra bởi Angular
-for file in /usr/share/nginx/html/*.js; do
-  if [ -f "$file" ]; then
-    sed -i "s|__API_KEY__|${API_KEY}|g" $file
-    sed -i "s|__ENCRYPT_SECRET__|${ENCRYPT_SECRET}|g" $file
-  fi
+# Thay thế các placeholder trong TẤT CẢ file .js (recursive) được sinh ra bởi Angular
+find /usr/share/nginx/html -name "*.js" -type f | while read file; do
+  sed -i "s|__API_KEY__|${API_KEY}|g" "$file"
+  sed -i "s|__ENCRYPT_SECRET__|${ENCRYPT_SECRET}|g" "$file"
 done
+
+# Verify replacement (debug log)
+echo "[entrypoint] API_KEY length: ${#API_KEY}"
 
 # Chạy lệnh tiếp theo được truyền vào từ CMD của Dockerfile (ví dụ: nginx -g daemon off;)
 exec "$@"
